334a4f6c36887b4d1f02d11ea2445dec838bb66b
Tauhid hossen <tauhidhossen04@gmail.com>
2025-07-03 16:29:08 +0600

    server cors issue fix

--- dist/app.js
+++ dist/app.js
@@ -8,8 +8,10 @@ const cors_1 = __importDefault(require("cors"));
 const book_route_1 = __importDefault(require("./routes/book.route"));
 const borrow_route_1 = __importDefault(require("./routes/borrow.route"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: ['http://localhost:5173', 'https://library-frontend-one-iota.vercel.app'],
+}));
 app.use('/api/books', book_route_1.default);
 app.use('/api/borrow', borrow_route_1.default);
 app.get('/', (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,11 @@ import bookRoutes from './routes/book.route';
 import borrowRoutes from './routes/borrow.route';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173','https://library-frontend-one-iota.vercel.app'],
+}));
+
 
 
 app.use('/api/books', bookRoutes);
