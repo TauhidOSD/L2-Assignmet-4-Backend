04ace8cf42d095a58aa67c6d50bffc8d82837823
Tauhid hossen <tauhidhossen04@gmail.com>
2025-07-03 11:49:57 +0600

    Server all is ok

--- src/routes/borrow.route.ts
+++ src/routes/borrow.route.ts
@@ -3,9 +3,11 @@ import { borrowBook, getBorrowSummary } from '../controllers/borrow.controller';
 
 const router = express.Router();
 
-
-
 router.post('/:bookId', borrowBook);
 router.get('/summary', getBorrowSummary);
 
 export default router;
+
+
+
+
